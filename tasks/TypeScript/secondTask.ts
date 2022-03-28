2002014e5a48fcf883d3f761da533610cdec7507
Andrei Chigileichik <andrei.chigileichik@intexsoft.by>
2022-03-28 11:52:25 +0300

    clean up code and optimize

--- tasks/TypeScript/secondTask.ts
+++ tasks/TypeScript/secondTask.ts
@@ -23,17 +23,14 @@ abstract class Resource<T extends Payload> {
     protected constructor(name: string, data: Data<T>[]) {
         this.name = name;
         this.data = data;
-
     }
 
     public getPayloadData(topic: string) {
-        const payloads = this.data.filter(data => data.topic === topic)[0]?.payloads
-        if (payloads) {
-            return [`Resource: ${this.name} Payload: ${payloads.map(item => JSON.stringify(item))}`];
-        }
+        const payloads = this.data.filter(data => data.topic === topic)[0]?.payloads;
         if (!payloads) {
             return [];
         }
+        return [`Resource: ${this.name} Payload: ${payloads.map(item => JSON.stringify(item))}`];
     }
 }
 
@@ -51,28 +48,65 @@ class Book extends Resource<BookPayload> {
 }
 
 class ResourceApplication {
-    private resources: Resource<Payload>[]
+    private resources: Resource<Payload>[];
 
     constructor(resources: Resource<Payload>[]) {
         this.resources = resources;
     }
 
     public printData(topic: string) {
-        const findingResources = this.resources.map(resource => {
-            return resource.getPayloadData(topic);
-        }).reduce((acc, val) => acc.concat(val), []);
+        const findingResources = this.resources.map(resource => resource.getPayloadData(topic)).reduce((acc, val) => acc.concat(val), []);
         const result = `Topic: ${topic} \n` + findingResources.join('\n');
         console.log(result);
     }
-
 }
 
-const site = new InternetResource('fimlov.net', [{
-    topic: 'films',
-    payloads: [{text: 'best film', url: 'best_films.net'}]
-}])
-const book = new Book('just a book', [{topic: 'films', payloads: [{page: 1, line: 10, text: 'bla-bla'},{page: 2, line: 40, text: 'blo-blo'}]}]);
-const book2 = new Book('book on sky', [{topic: 'sky', payloads: [{page: 1, line: 10, text: 'cool sky'}]}]);
+const site = new InternetResource('fimlov.net',
+    [
+            {
+                topic: 'films',
+                payloads:
+                    [
+                        {
+                            text: 'best film',
+                            url: 'best_films.net'
+                        }
+                    ]
+            }
+    ])
+const book = new Book('just a book',
+    [
+            {
+                topic: 'films',
+                payloads:
+                    [
+                        {
+                            page: 1,
+                            line: 10,
+                            text: 'bla-bla'
+                        },
+                        {
+                            page: 2,
+                            line: 40,
+                            text: 'blo-blo'
+                        }
+                    ]
+            }
+    ]);
 
+const book2 = new Book('book on sky',
+    [
+            {
+                topic: 'sky',
+                payloads:
+                    [
+                        {
+                            page: 1,
+                            line: 10,
+                            text: 'cool sky'
+                        }
+                    ]
+            }
+    ]);
 const app = new ResourceApplication([book, book2, site]);
 app.printData('films');
